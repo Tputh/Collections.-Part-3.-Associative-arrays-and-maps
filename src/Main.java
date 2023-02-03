19d67a9ee4d05fcba00047a6dc65bfe719bc359e
Имя <vitya.klyucherov@mail.ru>
2023-02-03 14:56:27 +0300

    Дз. Коллекции. Часть 3. Ассоциативные массивы и карты

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Коллекции. Часть 3. Ассоциативные массивы и карты");
 
+        Car car = new Car("Tayota");
+        Car car2 = new Car("Tayota");
+
+        Mechanic mechanic = new Mechanic( "Иван");
+        Mechanic mechanic1 = new Mechanic( "Иван");
+
+
+        Map<Car, Mechanic> list = new HashMap<>();
+        list.put(car, mechanic);
+        list.put(car2,  mechanic1);
+        System.out.println(list);
     }
 }
