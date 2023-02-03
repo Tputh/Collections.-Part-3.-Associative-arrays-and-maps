5a0cdcc15bcb8a707ce90d1259f7d46287bbe595
Имя <vitya.klyucherov@mail.ru>
2023-02-03 12:39:40 +0300

    Коллекции. Часть 3. Ассоциативные массивы и карты

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Коллекции. Часть 3. Ассоциативные массивы и карты");
+
+    }
+}
